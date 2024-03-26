a321b914aa493d3b3a61a180ebe9f6448ef851f2
AL Manar <AL Manar@DESKTOP-RGKRTGJ>
2024-03-27 00:08:32 +0300

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+namespace console
+{
+	class program
+	{
+		static void cheak(string path)
+		{
+			string[] data = File.ReadAllLines(path);
+			for (int i = 0; i < data.Length; i++)
+			{
+				if (data[i].StartsWith("//"))
+				{
+					data[i] = "";
+				}
+				if (data[i].StartsWith("/*"))
+				{
+					data[i] = "";
+				}
+				if (data[i].StartsWith("#define"))
+				{
+					data[i] = "";
+				}
+				if (data[i].EndsWith("*/"))
+				{
+					data[i] = "";
+				}
+			}
+			for (int i = 0; i < data.Length; i++)
+				Console.WriteLine(data[i]);
+
+
+		}
+		public static void Main()
+		{
+			string paths = @"C:\\Users\\AL Manar\\Desktop\\PS\\text.txt";
+			cheak(paths);
+
+		}
+	}
+}
